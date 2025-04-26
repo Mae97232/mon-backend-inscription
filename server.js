a5a364f323be6787ca0dec728acf3845512781eb
Yorick <maelyck97232@gmail.com>
2025-04-26 17:40:31 +0200

    modification de mes servers

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ app.post('/send-email', (req, res) => {
     port: 465,
     secure: true,
     auth: {
-      user: 'yorickspprt@gmail.com',
-      pass: 'dnltotxzjqsefgyr'
+      user: process.env.EMAIL_USER,
+      pass: process.env.EMAIL_PASS
     }
+    
   });
 
   const mailOptions = {
